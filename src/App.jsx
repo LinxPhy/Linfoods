4a83febd049cdd899686d601db32745d64a537b7
LinxPhy <lin.aboagye@gmail.com>
2023-06-21 19:27:08 +0100

    Added Masonry and Image ref file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
+import Masonry from 'react-masonry-css'
+import './Masonry.css'
 import './App.css'
+import Reference from './Reference'
 
 function App() {
 
+  const breakpointCols = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <div className="container">
+      <Masonry 
+        breakpointCols={breakpointCols}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        <div className="item">
+          <Image />
+        </div>
+      </Masonry>
+    </div>
   )
+}
+
+const Image = () => {
+
+  return(
+    <img src={Reference.butternut_squash_risotto.src} alt="fruit tart" />
+  )
+  
+
 }
 
 export default App
---
+++ src/Reference.js
@@ -0,0 +1,19 @@
+
+const Reference = {
+    butternut_squash_risotto: {
+        name: 'Butternut Squash Risotto',
+        src: '../src/images/butternut_squash_risotto.jpg'
+    },
+
+    fruit_tart1: {
+        name: 'Fruit Tart',
+        src: '../src/images/fruit_tart1.jpg'
+    },
+
+    fruit_tart2: {
+        name: 'Fruit Tart',
+        src: '../src/images/fruit_tart2.jpg'
+    }
+}
+
+export default Reference
